6b414160f13ee08f63a2c4dde952970764781f26
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-02-05 19:21:27 +0100

    feat: migrate normalize list

--- test/normalizeList.test.js
+++ test/normalizeList.test.js
@@ -1,28 +1,28 @@
 'use strict'
 
-const { test } = require('tap')
+const { test } = require('node:test')
 const { normalizeList } = require('../lib/normalizeList')
 
 test('normalizeList', function (t) {
   const testCases = [
-    [undefined, new Error('test must be array of strings or false')],
+    [undefined, new TypeError('test must be array of strings or false')],
     [false, []],
     [[], []],
     ['', ['']],
     [[''], ['']],
     [['a'], ['a']],
     ['a', ['a']],
-    [true, new Error('test must be array of strings or false')],
-    [1, new Error('test must be array of strings or false')],
-    [[1], new Error('test must be array of strings or false')]
+    [true, new TypeError('test must be array of strings or false')],
+    [1, new TypeError('test must be array of strings or false')],
+    [[1], new TypeError('test must be array of strings or false')]
   ]
   t.plan(testCases.length)
 
-  for (let i = 0; i < testCases.length; ++i) {
-    if (testCases[i][1] instanceof Error) {
-      t.throws(() => normalizeList(testCases[i][0], 'test'), testCases[i][1])
+  for (const testCase of testCases) {
+    if (testCase[1] instanceof Error) {
+      t.assert.throws(() => normalizeList(testCase[0], 'test'), testCase[1])
     } else {
-      t.strictSame(normalizeList(testCases[i][0], 'test'), testCases[i][1])
+      t.assert.deepStrictEqual(normalizeList(testCase[0], 'test'), testCase[1])
     }
   }
 })
